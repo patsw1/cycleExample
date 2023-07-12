6591a3edd787e82cdaf6b6b425640f359f59c10b
Patrick Sweeney <patsw@gmail.com>
2023-07-12 21:32:26 +0100

    code

---
+++ src/main/java/com/example/cycleExample/CycleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cycleExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycleExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cycleExample/CycleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cycleExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
